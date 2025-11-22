a6a935b891e4e329dbacb214f08674c7d646f276
gaddganepola <ganeypola@gmail.com>
2025-11-22 12:51:09 +0530

    initial project commit

---
+++ src/main/java/com/ddg/SpringSecSection02/SpringSecSection02Application.java
@@ -0,0 +1,13 @@
+package com.ddg.SpringSecSection02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecSection02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecSection02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddg/SpringSecSection02/SpringSecSection02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddg.SpringSecSection02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecSection02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
